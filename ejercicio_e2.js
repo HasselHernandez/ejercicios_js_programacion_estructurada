ce889d92de37bfbf5fd56fdae24e498f64b25857
HasselL <hassel3113r@gmail.com>
2025-08-14 23:37:29 -0600

    Se realizaron los ejercicios

---
+++ ejercicio_e2.js
@@ -0,0 +1,49 @@
+// 1. Declara una variable nombre y asignale tu nombre Muestra su valor en consola 
+
+let.nombre
+
+//
+
+let edad = 30;
+let ciudad = "Juigalpa";
+console.log(´´)
+
+const PI = 3.1416;
+console.log ("valor de PI: " + PI);
+
+let a = 50;
+let b = 70;
+let c = 90;
+let promedio = (a + b + c) / 3;
+console.log(promedio);
+
+let esMayorDeEdad = true;
+console.log("¿Es mayor de edad?" + esMayorDeEdad);
+
+let n1 = 15;
+let n2 = 25;
+let suma = n1 + n2;
+console.log (suma);
+
+let num1 = 100;
+let num2 = 20;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+console.log ("resta, multiplicacion, division");
+
+// 15.
+
+function esPar(numero) {
+  return numero % 2 ==0;
+
+}
+
+console.log (esPar(10));
+
+//19.
+
+let mayorDeDos =(num1, num2) => {
+  return num1 > num2 ? num1 : num2;
+}
+console.log(mayorDeDos(10,20));
